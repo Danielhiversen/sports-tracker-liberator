ae300d4edfdaa453d713ac2100bb134748ef23dd
Daniel Høyer Iversen <mail@dahoiv.net>
2016-12-25 15:02:25 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'endomondo',
+    packages = ['endomondo'],
+    version = '0.1',
+    description = 'Endomondo',
+    url='https://github.com/Danielhiversen/sports-tracker-liberator',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
